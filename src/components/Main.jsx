2f11b58e6eaee15820b2748130c2349e549a9732
Maylon-tech <minatokiroisen77@gmail.com>
2023-07-10 12:58:45 +0900

    finishing the Hero section

---
+++ src/components/Main.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { AiOutlineMail } from 'react-icons/ai'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
+
+const Main = () => {
+  return (
+    <div className='w-full h-screen text-center'>
+        <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
+            <div>
+                <p className='uppercase text-sm tracking-widest text-gray-60'>
+                    LETS BUILD SOMETHING TOGETHER
+                </p>
+                <h1 className='py-4 text-gray-700'>
+                    Hi, I'm <span className='text-[#5651e5]'>Mailon</span>
+                </h1>
+                <h1 className='py-2 text-gray-700'>
+                    A Front-End Web Developer
+                </h1>
+                <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
+                    I'm a front-end web developer specialized in building exceptional digital experiences. Currently, 
+                    I'm focused on building responsive front-end web applications while learning back-end technologies.
+                </p>
+
+                <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+                    <div className='rounded-full shadow-lg shadow-gray-400 ml-5 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <FaLinkedinIn size={25} />
+                    </div>
+                    <div className='rounded-full shadow-lg shadow-gray-400 ml-5 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <FaGithub size={25} />
+                    </div>
+                    <div className='rounded-full shadow-lg shadow-gray-400 ml-5 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <BsFillPersonLinesFill  size={25} />
+                    </div>
+                    <div className='rounded-full shadow-lg shadow-gray-400 ml-5 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <AiOutlineMail size={25} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Main
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
 import Navbar from '@/components/Navbar'
-
+import Main from '@/components/Main'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -14,6 +14,7 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Navbar />
+      <Main />
     </>
   )
 }
